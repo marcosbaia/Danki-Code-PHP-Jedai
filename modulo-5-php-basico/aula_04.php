4b1d9cf8a350ef944d5f895eb4dc1b35cc3326a9
Marcos Baia <cs.marcosbaia@gmail.com>
2020-07-09 21:09:22 -0300

    Create aula_04.php

---
+++ modulo-5-php-basico/aula_04.php
@@ -0,0 +1,18 @@
+<?php
+/*Tipos de Variáveis*/
+    /*String*/
+    $nome = "Marcos";
+    /*Inteiro*/
+    $idade = 35;
+
+    echo "Meu nome é ".$nome." tenho ".$idade." anos.";
+
+    /*Boolean (verdadeiro ou falso)*/
+    $bool = true;
+    /*Doubles*/
+    $double = 10.09;
+
+    echo "<br/> variavel boolean: ".$bool;
+    echo "<br/> variavel flutuante: ".$double;
+?>
+
