ac7467425089742110f0b8d615523c3812e5bb44
Marcos Baia <cs.marcosbaia@gmail.com>
2020-07-13 16:29:05 -0300

    Create aula_07.php

---
+++ modulo-5-php-basico/aula_07.php
@@ -0,0 +1,12 @@
+<?php
+    //Constante
+    define('NOME','Guilherme');
+    define('CIDADE','Cametá');
+
+    //variáveis
+    $idade = 23;
+    $nome = 'Marcos';
+
+    echo'Eu me chamo '.NOME.' e tenho '.$idade.' anos.<br/>';
+    echo"Eu me chamo $nome e sou de ".CIDADE;
+?>
