f4d09292566f68ab92f3fff262cb825efce5ffda
Marcos Baia <cs.marcosbaia@gmail.com>
2020-07-13 21:49:35 -0300

    Create aula_10.php

---
+++ modulo-5-php-basico/aula_10.php
@@ -0,0 +1,19 @@
+<?php
+    $valor1 = 10;
+    $valor2 = '10';
+
+    //Verifica se valores são iguais
+    echo 'O valor da variável 1 é: '.$valor1.'<br/>O valor da variável 2 é: '.$valor2.'<br/>';
+    if($valor1 == $valor2){
+        echo '<br/> Logo as duas variáveis são iguais <br/>';
+    }else{
+        echo '<br/> Logo as duas varáveis são diferentes <br/>';
+    }
+
+    //Verifica se valores são idênticos
+    if($valor1 === $valor2){
+        echo 'E são Idênticas';
+    }else{
+        echo 'Porêm NÃO são Idênticas';
+    }
+?>
