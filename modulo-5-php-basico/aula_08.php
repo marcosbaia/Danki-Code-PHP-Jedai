3a8ede029363648727acd4198d7b453ed6657d49
Marcos Baia <cs.marcosbaia@gmail.com>
2020-07-13 21:17:47 -0300

    Create aula_08.php

---
+++ modulo-5-php-basico/aula_08.php
@@ -0,0 +1,14 @@
+<?php
+    $soma = 6 + 5;
+    $subtracao = 6 - 5;
+    $multiplicação = 6 * 5;
+    $divisao = 6 / 5;
+    $resto = 6 % 5;
+
+    echo "<br/>A Soma de 6 + 5 = $soma <br/>";
+    echo "A Subtração de 6 - 5 = $subtracao <br/>";
+    echo "<br/>A Multiplicação de 6 * 5 = $multiplicação <br/>";
+    echo "A Divisão de 6 / 5 = $divisao <br/>";
+
+    echo '<br/> O Resto da Divisão de 6 / 5 = '.$resto;
+?>
