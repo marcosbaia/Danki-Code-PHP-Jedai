3167e99076631568596148806d0c3c567efd4158
Marcos Baia <cs.marcosbaia@gmail.com>
2020-07-13 21:49:32 -0300

    Create aula_09.php

---
+++ modulo-5-php-basico/aula_09.php
@@ -0,0 +1,10 @@
+<?php
+    $variavel1 = 'Guilherme';
+    $variavel2 = 'Guilherme';
+
+    if($variavel1 == $variavel2){
+        echo '<div style="width:300px; height:300px; background: blue;"></div>';
+    }else{
+        echo 'Condição NÃO satisfeita';
+    }
+?>
