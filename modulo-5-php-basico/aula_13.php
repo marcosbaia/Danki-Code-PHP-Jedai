e9fa2142b4b39e48cda8b6bc523c6611a20b4872
Marcos Baia <cs.marcosbaia@gmail.com>
2020-07-14 21:43:39 -0300

    Create aula_13.php

---
+++ modulo-5-php-basico/aula_13.php
@@ -0,0 +1,14 @@
+<?php
+
+// Comentário uma unica linha pode ser feita assim
+# Ou usando o Number Sign (Cerquilha), tbm conhecida como jogo da velha
+
+/*
+    Agora se você 
+    Deseja escrever
+    Um Bloco de comentário
+    o ideial é utilizar essa maneira
+*/
+
+
+?>
