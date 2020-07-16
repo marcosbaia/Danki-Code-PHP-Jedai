e072ff0f2f8293e0886ede23f4b76144d07fdc9a
Marcos Baia <cs.marcosbaia@gmail.com>
2020-07-15 23:59:58 -0300

    Create aula_07.php

---
+++ modulo-6-php-intermediario/aula_07.php
@@ -0,0 +1,31 @@
+<?php
+    $time = 'Flamengo';
+
+    switch($time){
+        case 'Flamengo':
+            echo $time.' é Clube de Futebol Carioca';
+            break;
+        case 'Fluminense':
+            echo $time.' é Clube de Futebol Carioca';
+            break;    
+        case 'Vasco':
+            echo $time.' é Clube de Futebol Carioca';
+            break;
+        case 'Botafogo':
+            echo $time.' é Clube de Futebol Carioca';
+            break;
+        case 'São Paulo':
+            echo $time.' é Clube de Futebol Paulista';
+            break;
+        case 'Corinthians':
+            echo $time.' é Clube de Futebol Paulista';
+            break;
+        case 'Palmeiras':
+            echo $time.' é Clube de Futebol Paulista';
+            break;
+        case 'Santos':
+            echo $time.' é Clube de Futebol Paulista';
+            break;
+    }
+
+?>
