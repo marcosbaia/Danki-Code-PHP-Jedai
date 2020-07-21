033b02568d9c738a5fd0076cd6ccae056f331005
Marcos Baia <cs.marcosbaia@gmail.com>
2020-07-20 22:53:57 -0300

    Create aula_10_cookies.php

---
+++ modulo-6-php-intermediario/aula_10_cookies.php
@@ -0,0 +1,10 @@
+
+<?php
+/* COOKIES
+    São pequenos ficheiros de texto que são guardados no computador do cliente, com o objectivo de armazenar pequenas informações. Os cookies são geridos pelos browsers: a maioria oferece ferramentas para ver e editar estes pequenos ficheiros, e permitem definir o ciclo de vida para um cookie.
+*/
+    session_star();
+    setcookie('nome','Guilherme',time()+(60*60*24),'/');
+
+    echo $_COOKIE['nome'];
+?>
