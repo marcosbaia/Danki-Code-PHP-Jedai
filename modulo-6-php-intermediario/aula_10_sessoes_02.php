7b146bed292ced53919739df84f0d49922a51495
Marcos Baia <cs.marcosbaia@gmail.com>
2020-07-20 22:54:07 -0300

    Create aula_10_sessoes_02.php

---
+++ modulo-6-php-intermediario/aula_10_sessoes_02.php
@@ -0,0 +1,12 @@
+<?php
+/* SESSÕES
+    Semelhante aos cookies, as sessões são, por predefinição, pequenos ficheiros de texto guardados numa pasta específica no servidor. Estes não podem ser requisitados pelo browser de forma direta, tendo de ser lidos com PHP. Com sessões, é possível guardar não só dados de tipos básicos, como strings e inteiros, mas também objectos de PHP, como classes, etc.
+*/
+    session_start(); //startou a sessão
+
+    //verificação da existencia da sessão
+    if(isset($_SESSION['nome'])){
+        echo $_SESSION['nome'];
+    }
+    
+?>
